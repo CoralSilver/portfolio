8a23ced241da6899d93f7e9ed8903013523df34e
CoralSilver <CoralSilver@users.noreply.github.com>
2019-12-01 16:11:46 -0500

    add rel="noreferrer" to social links

--- src/components/social/social.js
+++ src/components/social/social.js
@@ -10,22 +10,22 @@ const Social = () => {
   return (
     <SocialIcons>
       <li>
-        <a href="https://github.com/CoralSilver" target="_blank" aria-label="github icon">
+        <a rel="noreferrer" href="https://github.com/CoralSilver" target="_blank" aria-label="github icon">
           <GithubIcon />
         </a>
       </li>
       <li>
-        <a href="https://www.linkedin.com/pub/coral-silverman/28/b50/63" target="_blank" aria-label="linkedin icon">
+        <a rel="noreferrer" href="https://www.linkedin.com/pub/coral-silverman/28/b50/63" target="_blank" aria-label="linkedin icon">
           <LinkedinIcon />
         </a>
       </li>
       <li>
-      <a href="http://codepen.io/coralsea/" target="_blank" aria-label="codepen icon">
+      <a rel="noreferrer" href="http://codepen.io/coralsea/" target="_blank" aria-label="codepen icon">
           <CodepenIcon/>
         </a>
       </li>
       <li>
-        <a href="https://twitter.com/coral_silverman" target="_blank" aria-label="twitter icon">
+        <a rel="noreferrer" href="https://twitter.com/coral_silverman" target="_blank" aria-label="twitter icon">
           <TwitterIcon />
         </a>
       </li>
